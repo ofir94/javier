d6d57200676f886924e4173620d16081f3b09cf8
Nono9515 <guevol900@gmail.com>
2018-03-09 13:40:21 -0500

    sqlite maybe done

---
+++ src/assets/dummyDumb.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS devoloper(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, skill TEXT, yearsOfExperiencie INT);
+INSERT INTO developer(name, skill,yearsOfExperiencie) VALUES ("Javier","Ionic",'4');
+INSERT INTO developer(name, skill,yearsOfExperiencie) VALUES ("Ofir","YII",'8');
+INSERT INTO developer(name, skill,yearsOfExperiencie) VALUES ("Anabel","Java",'2');
