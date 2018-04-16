13abab4194d53cb845e27886a7449d8ec4bd6ca3
Ofir <ofir4991@gmail.com>
2018-04-16 14:48:44 -0400

    prueba bd 3

--- src/assets/bedbooking.sql
+++ src/assets/bedbooking.sql
@@ -1,31 +1,31 @@
 
-DROP TABLE IF EXISTS "reservation";
-CREATE TABLE "reservation" (
-"id_reservation"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-"from_date"  TEXT,
-"to_date"  TEXT,
-"cant_adult"  INTEGER,
-"cant_kid"  INTEGER,
-"price"  REAL,
-"deposit"  REAL,
-"comment"  TEXT,
-"id_room"  INTEGER,
-"status"  INTEGER,
-"id_client"  INTEGER,
+DROP TABLE IF EXISTS reservation;
+CREATE TABLE reservation (
+id_reservation  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+from_date  TEXT,
+to_date  TEXT,
+cant_adult  INTEGER,
+cant_kid  INTEGER,
+price  REAL,
+deposit  REAL,
+comment  TEXT,
+id_room  INTEGER,
+status  INTEGER,
+id_client  INTEGER,
 
 );
 
 
-DROP TABLE IF EXISTS "reservation_status";
-CREATE TABLE "reservation_status" (
-"id_status"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-"status"  TEXT,
-"color"  TEXT,
-"default"  INTEGER
+DROP TABLE IF EXISTS reservation_status;
+CREATE TABLE reservation_status (
+id_status  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+status  TEXT,
+color  TEXT,
+default  INTEGER
 );
 
-INSERT INTO "reservation_status" VALUES (1, 'Falta de Pago', null, null);
-INSERT INTO "reservation_status" VALUES (2, 'Depósito Pagado', null, null);
-INSERT INTO "reservation_status" VALUES (3, 'Totalmente Pagada', null, null);
-INSERT INTO "reservation_status" VALUES (4, 'Cancelado', null, null);
-INSERT INTO "reservation_status" VALUES (5, 'No disponible', null, null);
+INSERT INTO reservation_status VALUES (1, 'Falta de Pago', null, null);
+INSERT INTO reservation_status VALUES (2, 'Depósito Pagado', null, null);
+INSERT INTO reservation_status VALUES (3, 'Totalmente Pagada', null, null);
+INSERT INTO reservation_status VALUES (4, 'Cancelado', null, null);
+INSERT INTO reservation_status VALUES (5, 'No disponible', null, null);
