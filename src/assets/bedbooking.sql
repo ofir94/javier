9b4bca0abab3f8856c514b3a9bddb1ceb267bd35
Ofir <ofir4991@gmail.com>
2018-04-16 17:59:17 -0400

    default deleted form db

--- src/assets/bedbooking.sql
+++ src/assets/bedbooking.sql
@@ -8,13 +8,13 @@ CREATE TABLE IF NOT EXISTS  profile (id_profile  TEXT,name_business  TEXT,countr
 
 CREATE TABLE IF NOT EXISTS  reservation (id_reservation  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,from_date  TEXT,to_date  TEXT,cant_adult  INTEGER,cant_kid  INTEGER,price  REAL,deposit  REAL,comment  TEXT,id_room  INTEGER,status  INTEGER,id_client  INTEGER);
 
-CREATE TABLE IF NOT EXISTS  reservation_status (id_status  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,status  TEXT,color  TEXT,default  INTEGER);
+CREATE TABLE IF NOT EXISTS  reservation_status (id_status  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,status  TEXT,color  TEXT);
 
-INSERT INTO reservation_status VALUES (1, 'Falta de Pago', null, null);
-INSERT INTO reservation_status VALUES (2, 'Depósito Pagado', null, null);
-INSERT INTO reservation_status VALUES (3, 'Totalmente Pagada', null, null);
-INSERT INTO reservation_status VALUES (4, 'Cancelado', null, null);
-INSERT INTO reservation_status VALUES (5, 'No disponible', null, null);
+INSERT INTO reservation_status VALUES (1, 'Falta de Pago', null);
+INSERT INTO reservation_status VALUES (2, 'Depósito Pagado', null);
+INSERT INTO reservation_status VALUES (3, 'Totalmente Pagada', null);
+INSERT INTO reservation_status VALUES (4, 'Cancelado', null);
+INSERT INTO reservation_status VALUES (5, 'No disponible', null);
 
 CREATE TABLE IF NOT EXISTS  room (id_room  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,name  TEXT,cant_people  INTEGER,cant_bed_aditional  INTEGER,cant_bed_single  INTEGER,cant_bed_double  INTEGER,view_order  INTEGER);
 
