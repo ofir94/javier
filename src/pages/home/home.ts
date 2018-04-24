5ee19373490ad06f636cf0df6d165f16e4375410
Ofir <ofir4991@gmail.com>
2018-04-24 16:55:54 -0400

    showing reserv-client-detalles 4

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -225,38 +225,30 @@ export class HomePage {
   // BD
 
 // BD
-   getClientById(id){
+  async  getClientById(id){
     alert('client by id');
-    alert(id);
+    alert(id)
+       await this.databaseProvider.getClientById(id).then(data => {
+          alert('client found');
+         for(let dat of data) {
+           alert(dat.toString());
+           HomePage.client.id_client = dat.id_client;
+           HomePage.client.name = dat.name;
+           HomePage.client.address = dat.address;
+           HomePage.client.address2 = dat.address2;
+           HomePage.client.state = dat.state;
+           HomePage.client.postal_code = dat.postal_code;
+           HomePage.client.country = dat.country;
+           HomePage.client.passport = dat.passport;
+           HomePage.client.identification = dat.identification;
+           HomePage.client.phone = dat.phone;
+           HomePage.client.email = dat.email;
 
-     this.databaseProvider.getDatabaseState().subscribe(rdy => {
-       if (rdy) {
-         alert('ready')
-         this.databaseProvider.getClientById(id).then(data => {
-           alert('client found');
-           for (let dat of data) {
-             alert(dat.toString());
-             HomePage.client.id_client = dat.id_client;
-             HomePage.client.name = dat.name;
-             HomePage.client.address = dat.address;
-             HomePage.client.address2 = dat.address2;
-             HomePage.client.state = dat.state;
-             HomePage.client.postal_code = dat.postal_code;
-             HomePage.client.country = dat.country;
-             HomePage.client.passport = dat.passport;
-             HomePage.client.identification = dat.identification;
-             HomePage.client.phone = dat.phone;
-             HomePage.client.email = dat.email;
+           alert('finish for client by id')
+         }
 
-             alert('finish for client by id')
-           }
-
-         });
-       }
-     });
+        });
   }
-
-
   // BD
 // ESTATICO
   // getClientById(id){
